cd78497dbc6757a44e2051bc972ada95e25c1157
Mellieejj <Melanieverheul1992@gmail.com>
2020-04-27 14:57:46 +0200

    setup express app

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const port = process.env.PORT || 4000
+
+
+app.listen(port, () =>{
+  console.log(`app listen on ${port}`)
+})
